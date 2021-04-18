962526eabd3e1c7d8b6bb2b1632165fe9524c9bd
yijie <ywang@decibel.com>
2021-04-18 21:00:27 +0100

    cov matrix function added

--- app.py
+++ app.py
@@ -4,6 +4,84 @@ from flask_socketio import SocketIO, emit
 from kafka import KafkaProducer, KafkaConsumer, TopicPartition
 import uuid
 
+from io import StringIO
+import re
+
+# from calculate import parse_number, filter_input
+
+# from flask import Flask
+# from flask import request
+# from flask import jsonify
+# import json
+from io import StringIO
+
+import pandas as pd
+
+import numpy as np
+import re
+
+
+def parse(x):
+    """
+	The parser of pairs
+	"""
+
+    y = re.search("\((.*),(.*)\)", x).group(1, 2)
+
+    if y:
+        return y[0], y[1]
+
+    return None, None
+
+
+def parse_number(x):
+    """
+	Accept int and float only
+	"""
+
+    try:
+        y = re.match("^\((\d*\.?\d*),(\d*\.?\d*)\),?$", x).group(1, 2)
+
+        if y:
+            return y[0], y[1]
+    except AttributeError:
+        return None, None
+
+
+def catch(func, handle=lambda e: e, show_err=True, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except Exception as e:
+        if not show_err:
+            return handle(e)
+        else:
+            pass
+
+
+def convt_float(x):
+    try:
+        return float(x[0]), float(x[1])
+    except ValueError:
+        return False
+
+
+def filter_input(x):
+    """
+	Turn list of tuple to 2 den df
+	Filter out None from the raw input
+	Convert each column to number
+	Return numpy array
+	"""
+
+    df = pd.DataFrame(x, columns=["A", "B"])
+    df = df[~df["A"].isnull()]
+    df["A"] = pd.to_numeric(df["A"])
+    df["B"] = pd.to_numeric(df["B"])
+    # result = df.to_records(index=False)
+    result = df.to_numpy()
+    return result
+
+
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 cors = CORS(app)
@@ -58,8 +136,15 @@ def kafkaproducer(message):
         value=bytes(str(message), encoding="utf-8"),
         key=bytes(str(uuid.uuid4()), encoding="utf-8"),
     )
+
+    raw_input = [parse_number(message)]
+    final_input = filter_input(raw_input)
+    cov_matrix = np.cov(final_input)
+    # jason.dumps only accpept str, convert result into str
+    result = np.array2string(cov_matrix, precision=16)
+
     emit("logs", {"data": "Added " + message + " to topic"})
-    emit("kafkaproducer", {"data": message})
+    emit("kafkaproducer", {"data": message + " covariance matrix " + result})
     producer.close()
     kafkaconsumer(message)
 
